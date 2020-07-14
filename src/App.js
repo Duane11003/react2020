35c517446436ab3aeb184260375bf9ff8d72b7a6
duane11003 <duane.mcfar@gmail.com>
2020-07-13 23:40:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import Box from './Box'
 
 function App() {
-    return (
-        <div>
-            <p>Hello World</p>
-        </div>
-    )
+ return (
+   <div>
+     <p>hello world</p>
+     <Box height={10} width={10} color='red' />
+   </div>
+ )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
---
+++ src/Box.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Box(props) {
+  return (
+    <div>
+      <div
+        style={{
+          height: `${props.height}em`,
+          width: `${props.width}em`,
+          backgroundColor: props.color,
+        }}
+      ></div>
+    </div>
+  );
+}
+
+export default Box;
